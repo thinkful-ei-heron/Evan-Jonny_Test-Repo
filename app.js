9fdf9592dbe065c9e4d3544a463d22bd184be0d6
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2019-09-16 13:30:12 -0600

    yeet

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 let myTeam = "Evan , Jonny";
-console.log (myTeam);
-console.log(5+7);
-console.log (12+4);
+console.log(myTeam);
+
+function yearOfBirth(age) {
+    if (age < 0) {
+        throw new Error("Age can not be negative");
+    }
+    return 2019 - age;
+}
+
+function createGreeting(name, age) {
+    if (typeof age != "number" || typeof name != "string") {
+        throw new Error("Arguments not valid.");
+    }
+    const yob = yearOfBirth(age);
+    return 'Hi, my name is ' + name + ' and I\'m ' + age + ' years old.';
+}
+
+try {
+    const greeting1 = createGreeting("hello", 'asd');
+    console.log(greeting1);
+} catch (e) {
+    console.error(e);
+}
+
